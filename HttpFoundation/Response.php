d7198c29eb5a35a944a1da13b447d46bef14554d
Erik Duindam <erik@erikduindam.com>
2013-02-10 20:33:19 +0100

    Updated phpdoc

--- HttpFoundation/Response.php
+++ HttpFoundation/Response.php
@@ -13,26 +13,51 @@ use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 /**
  * Extended response containing raw data/objects returned from Controllers
+ *
+ * Erik Duindam <erik.duindam@morket.com>
  */
 class Response extends SymfonyResponse
 {
+    /**
+     * @var array
+     */
     protected $result;
 
+    /**
+     * Set Controller result. To support RedirectResponses, data is optional.
+     *
+     * @param array $result
+     */
     public function setResult(array $result = null)
     {
         $this->result = $result;
     }
 
+    /**
+     * Get Controller result
+     *
+     * @return array
+     */
     public function getResult()
     {
         return $this->result;
     }
 
+    /**
+     * Whether it contains an array with only one element
+     *
+     * @return bool
+     */
     public function hasSingleResult()
     {
         return count($this->result) == 1;
     }
 
+    /**
+     * Return only element in result array
+     *
+     * @return mixed
+     */
     public function getSingleResult()
     {
         return current($this->result);
